907d881f17900cde43e706402fb8b8f0cc009fbc
rtg <rtg1539@naver.com>
2024-09-05 20:26:12 +0900

    initial commit

---
+++ src/main/java/clush/api/ClushApiApplication.java
@@ -0,0 +1,13 @@
+package clush.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClushApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClushApiApplication.class, args);
+    }
+
+}
