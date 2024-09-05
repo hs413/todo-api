3be57f7b70b9c24831ae7fee4af76d502842616b
rtg <rtg1539@naver.com>
2024-09-06 08:36:07 +0900

    feat: 토큰 필터

---
+++ src/main/java/clush/api/auth/AccessTokenFilter.java
@@ -0,0 +1,93 @@
+package clush.api.auth;
+
+import clush.api.common.exception.CustomException;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.security.SignatureException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Log4j2
+@RequiredArgsConstructor
+@Component
+public class AccessTokenFilter extends OncePerRequestFilter {
+
+    private final JwtUtil jwtUtil;
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+        String path = request.getRequestURI();
+        return path.startsWith("/swagger-ui") ||
+                path.startsWith("/v3/api-docs") ||
+                (path.startsWith("/accounts") && !request.getMethod().equals("DELETE"));
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain) throws ServletException, IOException {
+
+        String token = extractToken(request);
+
+        if (token != null) {
+            try {
+                Map<String, Object> claim = validateAccessToken(token);
+
+                List<GrantedAuthority> authorities =
+                        List.of(new SimpleGrantedAuthority("ROLE_USER"));
+
+                UsernamePasswordAuthenticationToken authentication =
+                        new UsernamePasswordAuthenticationToken(claim, null, authorities);
+                authentication.setDetails(
+                        new WebAuthenticationDetailsSource().buildDetails(request));
+
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            } catch (CustomException customException) {
+                customException.sendResponseError(response);
+                return;
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String extractToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+
+    private Map<String, Object> validateAccessToken(String tokenStr)
+            throws CustomException {
+        try {
+            return jwtUtil.validateToken(tokenStr);
+        } catch (MalformedJwtException malformedJwtException) {
+            log.error("MalFormedJwtException-------------");
+            throw new CustomException(TokenErrorCode.MALFORMED_TOKEN);
+        } catch (SignatureException signatureException) {
+            log.error("SignatureException-------------");
+            throw new CustomException(TokenErrorCode.MALFORMED_TOKEN);
+        } catch (ExpiredJwtException expiredJwtException) {
+            log.error("ExpiredJwtException-------------");
+            throw new CustomException(TokenErrorCode.EXPIRED_TOKEN);
+        }
+    }
+
+}
--- src/main/java/clush/api/config/SecurityConfig.java
+++ src/main/java/clush/api/config/SecurityConfig.java
@@ -1,11 +1,13 @@
 package clush.api.config;
 
+import clush.api.auth.AccessTokenFilter;
 import java.util.Arrays;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -13,6 +15,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -22,6 +25,8 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @RequiredArgsConstructor
 @Log4j2
 public class SecurityConfig {
+
+    private final AccessTokenFilter accessTokenFilter;
 
     @Value("${security.origins}")
     private String origins;
@@ -35,21 +40,27 @@ public class SecurityConfig {
                 .sessionManagement(
                         session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
+        http.authorizeHttpRequests(requests -> requests
+                .requestMatchers(HttpMethod.DELETE, "/accounts/**").authenticated()
+                .requestMatchers("/accounts/**", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                .anyRequest().authenticated());
+
+        http.addFilterBefore(accessTokenFilter, UsernamePasswordAuthenticationFilter.class);
+
         return http.build();
     }
 
     /**
      * 비밀번호 인코더(해시)
-     * */
+     */
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
     /**
-     * CORS 설정<br/>
-     * origins는 yml 파일에 정의된 값을 가져 옴
-     * */
+     * CORS 설정<br/> origins는 yml 파일에 정의된 값을 가져 옴
+     */
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
